0a12b5cd6021ac4aba7d190e9eb6f134bfddaffa
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2024-07-22 21:28:52 +0800

    Add comment to classifier button

--- src/components/Button/Classifier/index.jsx
+++ src/components/Button/Classifier/index.jsx
@@ -1,3 +1,12 @@
+/**
+ * classifier component
+ * renders a customizable classifier button.
+ *
+ * props:
+ * - icon (string): The icon that will be used using Google Material Icons.
+ * - classifier (string): The name of the classifier.
+ */
+
 const Index = ({ icon, classifier }) => {
 	return (
 		<div className="border-primary-100 bg-background-100 text-text-100 hover:bg-primary-100 hover:text-white cursor-pointer border-2 w-[520px] h-[320px] rounded-[24px] flex flex-col gap-y-[16px] justify-center items-center">

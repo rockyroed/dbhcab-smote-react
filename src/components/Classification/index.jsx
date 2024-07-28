1a5b0bb4f4a8e84c268b3af20ea97b4839a83e25
johnreeey <107802114+johnreeey@users.noreply.github.com>
2024-07-28 16:09:36 +0800

    Classification Container

--- src/components/Classification/index.jsx
+++ src/components/Classification/index.jsx
@@ -1,10 +1,18 @@
+/**
+ * Classification Result Classifer
+ * Renders a customizable classifer
+ * 
+ * props:
+ * - sampling (string): the name of the sampling technique
+ */
 import ClassificationContainer from "./ClassificationContainer";
 
-const Index = () => {
+const Index = ({sampling}) => {
     return (
-        <div className="flex flex-col items-center justify-center gap-y-[16px]">
-            <span>DBHCAB-SMOTE</span>
-            <ClassificationContainer />
+        <div className="flex flex-col items-center justify-center gap-y-[12px]">
+            <span className="font-bold text-[20px]">{sampling}</span>
+            <ClassificationContainer percentage1='90.60' percentage2='9.40' classification1='toxic'
+            classification2='non-toxic' conWidth='450' conHeight='400'/>
         </div>
     )
 }

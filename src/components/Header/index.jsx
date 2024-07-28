13c6296c5fbc9b595e19d5ac4f608a7fd594c6d9
johnreeey <107802114+johnreeey@users.noreply.github.com>
2024-07-28 16:05:41 +0800

    added home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,14 +9,22 @@
  * - size (string): The size of the titles
  */
 
-const Index = ({title1, title2, home, size}) => {
+const Index = ({title1, title2, size, home}) => {
     return (
         <div className="flex flex-row items-center justify-center w-full">
             {home ? (
-                <div></div>
+                <div className="flex flex-col items-center justify-center gap-y-[30px] p-20">
+                <span className="font-extrabold text-5xl">
+                    <span className="text-7xl text-primary-100">DB</span>
+                    HCAB-SMOTE
+                </span>
+                <span>
+                    Please choose a classifier
+                </span>
+            </div>
             ) : (
-            <div className="flex flex-row">
-                <h1 className={`font-semibold text-[${size}px] text-primary-100`}>{title1}</h1>
+            <div className="flex flex-row ">
+                <h1 className={`font-semibold text-[${size}px] text-primary-100 uppercase`}>{title1}</h1>
                 <h1 className={`font-semibold text-[${size}px] uppercase`}>{title2}</h1>
             </div>
             )}

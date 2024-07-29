21cdc5ac09db98c4ec5c6d3217e1e418fd8c5c4e
Trisha Mae Erandio <erandiotrisha@gmail.com>
2024-07-29 18:11:10 +0800

    Home page (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Classification from "./components/Classification";
+import Header from "./components/Header"
+import Classifier from "./components/Button/Classifier"
 
 const App = () => {
 	return (
-		<Classification
-			sampling="DBHCAB-SMOTE"
-			percentage1="59.60"
-			percentage2="40.40"
-			classification1="toxic"
-			classification2="non-toxic"
-		/>
+		<div className="flex flex-col items-center mt-[64px]">
+			<Header home={true}/>
+			<div className="flex gap-5 mt-[64px]">
+				<Classifier icon="chat_error" classifier="TOXIC COMMENT"/>
+				<Classifier icon="voice_over_off" classifier="HATE SPEECH"/>
+			</div>
+		</div>
+		
 	);
 };
 

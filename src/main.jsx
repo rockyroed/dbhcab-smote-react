0199eb96c428b9e8049b17849a9dc0a487fb78ae
johnjohnjohn23 <slapmeonce101@gmail.com>
2024-07-29 23:40:59 +0800

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import ToxicComment from "./pages/ToxicComment"
 import HateSpeech from "./pages/HateSpeech"
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />
+	},
+	{
+		path: "/toxic-comment",
+		element: <ToxicComment />
 	},
 	{
 		path: "/hate-speech",
